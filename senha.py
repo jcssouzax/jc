68022140f25f3f26814d44fd75f96cc34685ad1f
jcssouzax <https.julio.cesar285@gmail.com>
2025-08-11 17:42:55 -0300

    Add files via upload

---
+++ senha.py
@@ -0,0 +1,12 @@
+senha=input('digite a sua senha')
+
+senhacorreta='casarosa' 
+
+print(senha==senhacorreta)
+
+#SE
+if senha==senhacorreta:
+     print('acesso permitido')
+#SENAO
+else:
+    print('acesso negado')
