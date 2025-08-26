a1725530321619c81dff3bc8325efaee77aaaef0
jcssouzax <https.julio.cesar285@gmail.com>
2025-08-26 17:40:17 -0300

    Add files via upload

---
+++ jc39.py
@@ -0,0 +1,10 @@
+def função (nome,horario):
+    if horario=='tarde':
+        print(nome,'tarde')
+    if horario==('manha'):
+        print(nome,'manha')
+    if horario=='noite':
+        print(nome,'noite')
+
+
+função ('julio','manha')
---
+++ jc40.py
@@ -0,0 +1,13 @@
+def função (n1,n2 ,n3):
+    if n3=='+':
+        print(n1+n2)
+    if n3=='-':
+        print(n1-n2)
+    if n3=='*':
+        print(n1*n2)
+    if n3=='/':
+        print(n1/n2)
+
+
+
+função(4,4,'+')
